ebfed10c7a892b703b7b2607800f4ebc026904df
deGekata <y.zvonkov@yandex.ru>
2022-04-10 02:31:20 +0300

    binheap

--- Structures/BinHeap.cpp
+++ Structures/BinHeap.cpp
@@ -10,13 +10,13 @@ struct binHeap {
     int64_t size;
     int64_t capacity;
     heap_type* data;
-    bool (*cmp_func)(heap_type, heap_type);
+    int (*cmp_func)(heap_type, heap_type);
 };
 
+typedef struct binHeap binHeap;
 
 
-
-binHeap* createHeap(heap_type* data, int64_t size);
+// binHeap* createHeap(heap_type* data, int64_t size);
 
 inline int64_t heapLeftSon(int64_t index);
 
@@ -34,42 +34,42 @@ heap_type heapExtractTop(binHeap* heap);
 
 
 
-void swap(int16_t* lft, int16_t* rht) {
+void swap16(int16_t* lft, int16_t* rht) {
     int16_t temp = *lft;
     *lft = *rht;
     *rht = temp;
     return;
 }
 
-void swap(int32_t* lft, int32_t* rht) {
+void swap32(int32_t* lft, int32_t* rht) {
     int32_t temp = *lft;
     *lft = *rht;
     *rht = temp;
     return;
 }
 
-void swap(int64_t* lft, int64_t* rht) {
+void swap64(int64_t* lft, int64_t* rht) {
     int64_t temp = *lft;
     *lft = *rht;
     *rht = temp;
     return;
 }
 
-void swap(float* lft, float* rht) {
+void swapfl(float* lft, float* rht) {
     float temp = *lft;
     *lft = *rht;
     *rht = temp;
     return;
 }
 
-void swap(double* lft, double* rht) {
+void swapdb(double* lft, double* rht) {
     double temp = *lft;
     *lft = *rht;
     *rht = temp;
     return;
 }
 
-binHeap* createHeap(heap_type* data, int64_t size, bool (*cmp_func)(heap_type, heap_type)) {
+binHeap* createHeapWithData(heap_type* data, int64_t size, int (*cmp_func)(heap_type, heap_type)) {
     binHeap* heap = (binHeap*) malloc(sizeof(binHeap));
     heap->cmp_func = cmp_func;
     int binpw = 1;
@@ -83,14 +83,14 @@ binHeap* createHeap(heap_type* data, int64_t size, bool (*cmp_func)(heap_type, h
     memcpy(heap->data + 1, data, sizeof(heap_type) * size);
 
     for (int i = size/2; i >= 1; --i) {
-        printf("lol %ld\n", i);
+        // printf("lol %ld\n", i);
         heapSiftDown(heap, i);
     }
     
     return heap;
 }
 
-binHeap* createHeap(int64_t size, bool (*cmp_func)(heap_type, heap_type)) {
+binHeap* createHeap(int64_t size, int (*cmp_func)(heap_type, heap_type)) {
     binHeap* heap = (binHeap*) malloc(sizeof(binHeap));
     heap->cmp_func = cmp_func;
     int binpw = 1;
@@ -103,6 +103,13 @@ binHeap* createHeap(int64_t size, bool (*cmp_func)(heap_type, heap_type)) {
     heap->data = (heap_type*) malloc(sizeof(heap_type) * binpw);
     
     return heap;
+}
+
+void heapDtor(binHeap* heap) {
+    free(heap->data);
+    heap->size = 0;
+    heap->capacity = 0;
+    return;
 }
 
 inline int64_t heapLeftSon(int64_t index) {
@@ -118,7 +125,7 @@ void heapSiftUp(binHeap* heap, int64_t index) {
     assert(heap && "heap ptr must not be NULL");
     assert(index <= heap->size);
     while (index > 1 && heap->cmp_func(heap->data[index / 2], heap->data[index])) {
-        swap(heap->data + index, heap->data + index / 2);
+        swap64(heap->data + index, heap->data + index / 2);
         index /= 2;
     }
     
@@ -137,11 +144,15 @@ void heapSiftDown(binHeap* heap, int64_t index) {
         if (heap->cmp_func(heap->data[dest], heap->data[index]) || heap->data[dest] == heap->data[index])
             break;
         
-        swap(heap->data + index, heap->data + dest);
+        swap64(heap->data + index, heap->data + dest);
         index = dest;
     }
     
     return;
+}
+
+size_t heapGetSize(binHeap* heap) {
+    return heap->size;
 }
 
 void heapInsert(binHeap* heap, heap_type elem) {
@@ -166,7 +177,7 @@ heap_type heapExtractTop(binHeap* heap) {
         heap->size--;
         return ret_val;
     }
-    swap(heap->data + 1, heap->data + heap->size);
+    swap64(heap->data + 1, heap->data + heap->size);
     heap->size--;
     heapSiftDown(heap, 1);
     return ret_val;
@@ -189,33 +200,83 @@ void heapPrint(binHeap* heap) {
     printf("}\n");
 }
 
-bool cmpl(heap_type lft, heap_type rht) {
+int cmpl(heap_type lft, heap_type rht) {
     return lft < rht;
 }
 
-bool cmpg(heap_type lft, heap_type rht) {
+int cmpg(heap_type lft, heap_type rht) {
     return lft > rht;
 }
 
 int main() {
-    size_t heaps_cnt = 0;
-    scanf("%lu", &heaps_cnt);
+    binHeap** heaps = (binHeap**) calloc(5, sizeof(binHeap*));
+    int heaps_cnt;
+    scanf("%d", &heaps_cnt);
 
-    binHeap* heaps = (binHeap*) calloc(heap)
-
-    heap_type arr[10] = {5, 2, 3, 1, 4};
-    binHeap* heap = createHeap(arr, 5, cmpg);
-    printf("heap size :%ld\n", heap->size);
-    heapPrint(heap);
-    while (heapSize(heap) != 0) {
-        printf("extracted min: %lu\n", heapExtractTop(heap));
-        heapPrint(heap);
-        printf("------------------------\n");
+    int oper_num, is_ok;
+    is_ok = scanf("%d", &oper_num);
+    
+    while (is_ok != -1) {
+        switch (oper_num) {
+        case 0: {
+            int num, size, type;
+            scanf("%d %d %d", &num, &size, &type);
+            if (!type) {
+                heaps[num] = createHeap(size, cmpg);
+            } else {
+                heaps[num] = createHeap(size, cmpl);
+            }
+            break;
+        }
+        case 1: {
+            int num, elem;
+            scanf("%d %d", &num, &elem);
+            heapInsert(heaps[num], elem);
+            break;
+        }
+        case 2:{
+            int num;
+            scanf("%d", &num);
+            printf("%ld\n", heapGetTop(heaps[num]));
+            break;
+        }
+        case 3:{
+            int num;
+            scanf("%d", &num);
+            heapExtractTop(heaps[num]);
+            break;
+        }
+        case 4:{
+            int num;
+            scanf("%d", &num);
+            printf("%ld\n", heapGetSize(heaps[num]));
+            break;
+        }
+        case 5:{
+            int num1, num2;
+            scanf("%d %d", &num1, &num2);
+            while (heapGetSize(heaps[num2])) {
+                heapInsert(heaps[num1], heapExtractTop(heaps[num2]));
+            }
+            
+            break;
+        }
+        case 6:{
+            int num;
+            scanf("%d", &num);
+            heapDtor(heaps[num]);
+            free(heaps[num]);
+            break;
+        }
+        default:
+            break;
+        }
+        is_ok = scanf("%d", &oper_num);
+        /* code */
     }
+    
+    free(heaps);
 
-    heapInsert(heap, 1);
-    heapPrint(heap);
-    // printf("%%");
     return 0;
 }
 
