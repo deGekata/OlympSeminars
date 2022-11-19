f814d6de9a3ac64c65ee4ee524f37c98a08a574e
deGekata <y.zvonkov@yandex.ru>
2022-11-19 20:10:42 +0300

    new solve

--- 3_sem/Base/Contest2/C-knight.cpp
+++ 3_sem/Base/Contest2/C-knight.cpp
@@ -55,7 +55,6 @@ public:
 int bfs_color(Graph_Adj& gr, int x0, int y0, int x1, int y1, std::vector<my_pair>& ans) {
 
     std::queue<my_pair> process;
-    // int max_comp_size = 0;
     int comp_cnt = 0;
     int bar = gr.V_cnt;
     process.push({(int16_t)x0, (int16_t)y0});
@@ -66,9 +65,8 @@ int bfs_color(Graph_Adj& gr, int x0, int y0, int x1, int y1, std::vector<my_pair
         my_pair cur_vert = process.front();
         int16_t x = cur_vert.x;
         int16_t y = cur_vert.y;
-        // std::cout << x << " " << y << " iter\n";
         process.pop();
-        // if (shift(y + 2, x + 1) >= gr.V_cnt * gr.V_cnt) exit(0);
+
         if (y-1>=0 && x - 2 >= 0 && gr.unused[shift(y - 1, x - 2)]) {
                 process.push((my_pair){x - 2, y - 1});
                 prev[y-1][x-2] = {x, y};
@@ -119,25 +117,21 @@ int bfs_color(Graph_Adj& gr, int x0, int y0, int x1, int y1, std::vector<my_pair
                 gr.unused[shift(y + 2, x + 1)] = false;
         }
     }
-    // for (auto it: gr.unused) {
-    //     std::cout << it << "\n";
-    // }
+
     int16_t cur_x = x1, cur_y = y1;
     
     for (; cur_x != x0 || cur_y != y0;) {
         ans.push_back(my_pair{cur_x, (int16_t)(cur_y + 1)});
-        // printChessPos(cur_x, cur_y + 1);
-        // std::cout << cur_x << " " << cur_y << "\n";
-
         my_pair tmp = prev[cur_y][cur_x];
         cur_x = tmp.x;
         cur_y = tmp.y;
     }
+
     std::cout << ans.size() << "\n";
     for (int it = ans.size() - 1; it >= 0; --it) {
         printChessPos(ans[it].x, ans[it].y);
     }
-    // std::cout << prev[y1][x1].x << "  " << prev[y1][x1].y << "\n";
+
     return comp_cnt;
 }
 
@@ -155,21 +149,10 @@ int main() {
     std::cin >> tmp_sym >> y1;
     x1 = tmp_sym - 'A';
     x_ = y_ = size;
-    // std::cout << '"' <<  tmp_sym << '"' << tmp_num;
-    // printChessPos(x0, y0);
-    // printChessPos(x1, y1);
     y0--, y1--;
-    // std::cin >> y >> x;
-    // std::unordered_map<int, int> components;
-    // std::vector<std::unordered_map<int, int>> components;
     Graph_Adj gr(size);
-    // gr.readAdjList(x, y);
-    // gr.printAdjList();
     std::vector<my_pair> ans;
     bfs_color(gr, x0, y0, x1, y1, ans);
-    // for (int it = 0; it < v_num; ++it) {
-    //     std::cout << components[it]<< " ";
-    // }
     std::cout << "\n";
     return 0;
 }
